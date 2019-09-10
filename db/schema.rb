b58da856d5816ef973ab193cea546af18cc31e42
rociac <brsrkdev@gmail.com>
2019-09-10 16:25:44 -0500

    Create post model and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_205207) do
+ActiveRecord::Schema.define(version: 2019_09_10_210609) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
