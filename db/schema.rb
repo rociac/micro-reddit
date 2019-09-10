e93147c3188c9d3c9f2d7010c9e0fd004bcba356
Adebola <onedebos@gmail.com>
2019-09-10 22:57:08 +0100

    create Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_210609) do
+ActiveRecord::Schema.define(version: 2019_09_10_212958) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
